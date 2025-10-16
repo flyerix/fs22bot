bd8c07d0a7cac539c6a2cdd08a7b97d5127121af
flyerix <154899257+flyerix@users.noreply.github.com>
2025-10-16 13:01:53 +0200

    Update keepAlive.js

--- keepAlive.js
+++ keepAlive.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const server = express();
 
+server.all("/", (req, res) => {
+  res.send("✅ Bot attivo!");
+});
+
+function keepAlive() {
+  server.listen(3000, () => {
+    console.log("🌐 Server web attivo su porta 3000");
+  });
+}
+
+module.exports = keepAlive;
